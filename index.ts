73c7c356779fae373318b3418be851f63d01f19b
“Chris <chrisanderson@developersinstitute.co.nz>
2021-07-27 14:25:14 +1200

    save point. api key seems to be stale, parking repo for now

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { User } from './src/classes/User';
+import { Company } from './src/classes/Company';
+import { CustomMap } from './src/classes/CustomMap';
+
+// Random Name and Company generated via console logs
+
+const user = new User();
+console.log(user);
+const company = new Company();
+console.log(company);
+
+new CustomMap('map');
+
+// ----------------------------------------------------------------
+
+
+
+
---
+++ src/classes/CustomMap.ts
@@ -0,0 +1,14 @@
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divID: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divID), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+}
