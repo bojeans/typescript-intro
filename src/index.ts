10fe72bd31f5b782071e64507ece2a030bf58e23
“Chris <chrisanderson@developersinstitute.co.nz>
2021-07-27 12:29:38 +1200

    coded up name, company. attempted displaying google map, not working atm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello world');
+import { User } from './classes/User';
+import { Company } from './classes/Company';
+
+// Random Name and Company generated via console logs
+
+const user = new User();
+console.log(user);
+const company = new Company();
+console.log(company);
+
+// ----------------------------------------------------------------
+
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
+
+
